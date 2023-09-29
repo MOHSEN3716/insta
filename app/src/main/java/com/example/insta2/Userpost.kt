02e59b9ced6f4cecb1277d1da10d6289f639d7e7
unknown <mohsen.3716.mohsen@gmail.com>
2023-09-30 03:00:00 +0330

    userpost don

--- app/src/main/java/com/example/insta2/Userpost.kt
+++ app/src/main/java/com/example/insta2/Userpost.kt
@@ -1,10 +1,11 @@
 package com.example.insta2
 
 import com.google.gson.annotations.SerializedName
+import java.net.URL
 
 class Userpost {
     @SerializedName("mostviewedvideos")
-    var postvideo:String?=null
+    var small_poster:String?=null
     var tag:String?=null
     var views:Int?=null
     var comments:Int?=null
--- app/src/main/java/com/example/insta2/postadabter.kt
+++ app/src/main/java/com/example/insta2/postadabter.kt
@@ -1,8 +1,11 @@
 package com.example.insta2
 
 import android.content.Context
+import android.widget.GridView
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import io.github.farshidroohi.AdapterRecyclerView
 
 class postadabter : AdapterRecyclerView<Userpost>(R.layout.post_itemm,0,0,0) {
@@ -14,7 +17,16 @@ class postadabter : AdapterRecyclerView<Userpost>(R.layout.post_itemm,0,0,0) {
     ) {
         var view=viewHolder.itemView
         var txtusername=view.findViewById<TextView>(R.id.txtusername)
+        var postmedia=view.findViewById<ImageView>(R.id.imageView2)
+
+
         txtusername.text= element?.username
+
+        Glide.with(context)
+            .load(element?.small_poster)
+            .placeholder(R.drawable.baseline_download_24)
+            .into(postmedia)
+
     }
 
 }
